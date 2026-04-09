ef79f9f75f15e96377c1d560b3d010a3622bd21a
Rishika Reddy <bodgamrishikareddy608@gmail.com>
2026-04-10 02:30:18 +0530

    fix cors error

--- server.ts
+++ server.ts
@@ -43,6 +43,7 @@ async function startServer() {
   app.use(cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:3000",
       "https://vi-notes-tgxf.vercel.app" // 🔥 REPLACE AFTER DEPLOY
     ],
     credentials: true
